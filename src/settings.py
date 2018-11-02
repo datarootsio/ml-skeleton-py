cecee50d60705161556b4e4e7dd6fabed31e2a9a
Bart6114 <bart@dataroots.io>
2018-11-02 12:32:27 +0100

    closes #24

--- src/settings.py
+++ src/settings.py
@@ -5,13 +5,13 @@ Most of these variables can be overridden through environment variables.
 
 import os
 
-DATA_RAW = os.getenv('DATA_RAW', './data/raw/')
-DATA_TRANSFORMED = os.getenv('DATA_TRANSFORMED', './data/transformed/')
-DATA_STAGING = os.getenv('DATA_TRANSFORMED', './data/staging/')
-DATA_PREDICTIONS = os.getenv('DATA_TRANSFORMED', './data/predictions/')
+DATA_RAW = os.path.abspath(os.getenv('DATA_RAW', './data/raw/'))
+DATA_TRANSFORMED = os.path.abspath(os.getenv('DATA_TRANSFORMED', './data/transformed/'))
+DATA_STAGING = os.path.abspath(os.getenv('DATA_TRANSFORMED', './data/staging/'))
+DATA_PREDICTIONS = os.path.abspath(os.getenv('DATA_TRANSFORMED', './data/predictions/'))
 
-MODEL_DIR = os.getenv('MODEL_DIR', './models')
-MODEL_METADATA_DIR = os.getenv('MODEL_METADATA_DIR', './models/metadata')
+MODEL_DIR = os.path.abspath(os.getenv('MODEL_DIR', './models'))
+MODEL_METADATA_DIR = os.path.abspath(os.getenv('MODEL_METADATA_DIR', './models/metadata'))
 
 FLASK_ENDPOINT_HOST = os.getenv('FLASK_ENDPOINT_HOST', '0.0.0.0')
 FLASK_ENDPOINT_PORT = os.getenv('FLASK_ENDPOINT_PORT', 5000)

2907c54c2dee65d920ff7292d4951774625d7519
stefan <stefan@dataroots.io>
2018-11-07 17:46:16 +0100

    Lint explainability script. (#14)

--- src/model/explainability.py
+++ src/model/explainability.py
@@ -1,3 +1,5 @@
+"""Model explainability helpers."""
+
 from sklearn.externals import joblib
 import pandas as pd
 from mlmonkey.metadata import ModelMetadata
@@ -8,9 +10,7 @@ class ModelExplainer(object):
     """Expose model explainability functions."""
 
     def __init__(self, metadata):
-        """
-        Load model, data and create explainer.
-        """
+        """Load model, data and create explainer."""
         if not isinstance(metadata, ModelMetadata):
             raise Exception('Invalid metadata format.')
 
@@ -19,18 +19,12 @@ class ModelExplainer(object):
             .drop(columns=['species', 'petal_length'])
         self.explainer = shap.TreeExplainer(self.model)
 
-    def get_ex(self):
-        return self.explainer
-
     def plot_feature_importance(self):
-        """Plots features importance, as bar plot."""
+        """Plot features importance, as bar plot."""
         shap_values = self.explainer.shap_values(self.train_data)
         shap.summary_plot(shap_values, self.train_data, plot_type='bar')
-
 
     def explain_prediction(self, dataframe):
         """Visualize explanation after calculating prediction(s) for given observation(s)."""
         shaps = self.explainer.shap_values(dataframe)
         return shap.force_plot(self.explainer.expected_value, shaps, dataframe)
-
-

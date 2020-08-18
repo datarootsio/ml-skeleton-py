b0f63f874af347fa7f9c8aefe18a042894ca8ed7
Wai Kit Tsang <waikit.ts4ng@gmail.com>
2020-08-18 15:36:52 +0200

    remove dynaconf and use settings.py in ml_skeleton_py

--- ml_skeleton_py/__init__.py
+++ ml_skeleton_py/__init__.py
@@ -6,18 +6,9 @@ Specify logger and load dotenv.
 import os
 import logging
 from dotenv import load_dotenv, find_dotenv
-from dynaconf import Dynaconf
 
 load_dotenv(find_dotenv())
 
 logging.basicConfig(level=os.getenv("LOG_LEVEL", "WARNING"))
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-
-settings = Dynaconf(
-    # export envvars with `export DYNACONF_FOO=bar`.
-    envvar_prefix="DYNACONF",
-    # Load files in the given order.
-    settings_files=["settings.yaml", ".secrets.yaml"],
-)

8ac00e13ac5d219cb7252c6bb455a1da577f9ac7
baturayo <baturayofluoglu@gmail.com>
2020-09-28 15:18:01 +0200

    refac train.py

--- ml_skeleton_py/model/train.py
+++ ml_skeleton_py/model/train.py
@@ -4,16 +4,19 @@ EXAMPLE of training procedure on a highly imbalanced credit fraud dataset.
 The dataset is retrieved from:
 https://www.kaggle.com/janiobachmann/credit-fraud-dealing-with-imbalanced-datasets.
 """
-
+import logging
 import os
+
+import joblib
+import numpy as np
 import pandas as pd
+import sklearn.pipeline
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import RobustScaler
-from sklearn.linear_model import LogisticRegression
-import logging
+
 from ml_skeleton_py import settings
-from ml_skeleton_py.helper import save_pickle
 
 logger = logging.getLogger(__name__)
 logging.getLogger().setLevel(logging.INFO)
@@ -23,7 +26,7 @@ def train(dataset: str, model_name: str = "lr") -> None:
     """
     Train models using X_train and y_train with a specific classifier.
 
-    Trains a specific classifier with a set of optimized hyperparameters
+    Trains a specific classifier with a set of optimized hyper-parameters
     in a 5fold-CV. The training results with the accompanying model is
     saved in ./models/
 
@@ -40,10 +43,10 @@ def train(dataset: str, model_name: str = "lr") -> None:
     """
     # loading data
     df = pd.read_csv(os.path.join(settings.DATA_TRANSFORMED, dataset))
-    X = df.drop("Class", axis=1)
-    y = df["Class"]
+    y = df.pop("Class")
+    X = df
 
-    # preprocessing
+    # pre-processing
     scaler = RobustScaler()
 
     # In this specific example logistic regression was chosen as
@@ -56,18 +59,38 @@ def train(dataset: str, model_name: str = "lr") -> None:
     # training
     pipeline.fit(X, y)
     training_score = cross_val_score(pipeline, X, y, cv=5, scoring="roc_auc")
-
     logger.info(f"Classifier: {pipeline.__class__.__name__}")
     logger.info(
         "Has a training score "
         + f"of {round(training_score.mean(), 2) * 100} % roc_auc"
     )
 
-    # saving
+    # Save model
+    dump_model(pipeline, model_name, training_score)
+
+
+def dump_model(pipeline: sklearn.pipeline, model_name: str, training_score: np.ndarray) -> None:
+    """
+    Serialized trained pipeline to ./models/ directory
+
+    Parameters:
+        pipeline (sklearn.pipeline): Fitted pipeline object that we want to serialize
+        model_name (str): Name of the model that we want to serialize
+                     default:
+                        "lr": logistic regression
+        training_score (np.ndarray): ROC AUC scores of each CV
+
+    Returns:
+        None
+    """
+
+    # Model
     pred_result = {
         "clf": model_name,
         "roc_auc": training_score.mean(),
         "model": pipeline,
     }
-    model_path = os.path.join(settings.MODEL_DIR, model_name) + ".p"
-    save_pickle(pred_result, model_path)
+    model_location = os.path.join(settings.MODEL_DIR, model_name) + ".joblib"
+    with open(model_location, "wb") as f:
+        # Serialize pipeline and compress it with the max factor 9
+        joblib.dump(pred_result, f, compress=9)

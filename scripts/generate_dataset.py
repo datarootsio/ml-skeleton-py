92dce926d2dd6780036e2630d84ce0369268974c
baturayo <baturayofluoglu@gmail.com>
2020-09-29 18:48:58 +0200

    adapt generate_dataset.py to new unit tests

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -1,6 +1,12 @@
 import argparse
+import logging
+import os
 
 from ml_skeleton_py import etl
+from ml_skeleton_py import settings as s
+
+logger = logging.getLogger(__name__)
+logging.getLogger().setLevel(logging.INFO)
 
 
 def generate() -> None:
@@ -10,7 +16,10 @@ def generate() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", default="creditcard.csv", help="raw dataset to generate train and test data")
     args = parser.parse_args()
-    etl.generate(args.dataset)
+
+    input_location = os.path.join(s.DATA_RAW, args.dataset)
+    output_location = os.path.join(s.DATA_TRANSFORMED, args.dataset)
+    etl.generate(input_location, output_location)
 
 
 if __name__ == "__main__":
